7ce03d3666e05996218958854b22404087a7aec8
David Boclé <bcldvd@gmail.com>
2016-12-21 16:13:30 +0100

    backdrop size fixes

--- src/gif-player.component.js
+++ src/gif-player.component.js
@@ -18,27 +18,41 @@
         };
     }
 
-    function gifPlayerCtrl() {
+    gifPlayerCtrl.$inject = ['$element'];
+    function gifPlayerCtrl($element) {
         var $ctrl = this;
 
         $ctrl.$onInit = $onInit;
 
         function $onInit() {
-            $ctrl.gifId = generatedId();
-            setTimeout(function () {
-                var second = new freezeframe({
-                    'selector': '#' + $ctrl.gifId,
-                    'animation_play_duration': Infinity,
-                    'non_touch_device_trigger_event': 'hover'
-                }).freeze();
-            }, 0);
-        }
+                $ctrl.gifId = generatedId();
+                    setTimeout(function() {
+                        var second = new freezeframe({
+                            'selector': '#' + $ctrl.gifId,
+                            'animation_play_duration': Infinity,
+                            'non_touch_device_trigger_event': 'hover'
+                        }).freeze();
+                        setHeightnWidth();
+                    },0);
+            }
 
-        function generatedId() {
-            var randomId = Math.random();
-            var prefix = 'id';
-            return prefix + randomId.toString().slice(2, randomId.length);
-        }
+            function generatedId() {
+                var randomId = Math.random();
+                var prefix   = 'id';
+                return prefix + randomId.toString().slice(2, randomId.length);
+            }
+
+            function setHeightnWidth() {
+                setTimeout(function () {
+                    var width  = $element.find('img')[0].width;
+                    var height = $element.find('img')[0].height;
+
+                    $element.css({
+                        width: width,
+                        heigth: height
+                    })
+                }, 100);
+            }
 
     }
 })();
